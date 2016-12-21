4b013bd16648848e679f7300c3fa0329da9ca824
weian wang <weian.wang@meridigen.com>
2016-12-21 20:59:59 +0800

    implement facebook authentication with firebaseui

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,44 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <div id="firebaseui-auth-container"></div>
   </div>
 </template>
 
 <script>
+import firebaseui from 'firebaseui'
+import firebase from 'firebase'
 import Hello from './components/Hello'
 
 export default {
   name: 'app',
   components: {
     Hello
+  },
+  mounted () {
+    const uiConfig = {
+      signInSuccessUrl: '<url-to-redirect-to-on-success>',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID
+      ],
+      // Terms of service url.
+      tosUrl: '<your-tos-url>'
+    }
+    const ui = new firebaseui.auth.AuthUI(firebase.auth())
+    ui.start('#firebaseui-auth-container', uiConfig)
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="sass">
+@import '../node_modules/firebaseui/dist/firebaseui.css'
+#app
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,25 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Firebase from 'firebase'
+import VueFire from 'vuefire'
+
+Vue.use(VueFire)
+
+var config = {
+  apiKey: 'AIzaSyB53Hvjn48RgWg2WnmGGRIB9eeWTlsQQH4',
+  authDomain: 'birs-tree.firebaseapp.com',
+  databaseURL: 'https://birs-tree.firebaseio.com',
+  storageBucket: 'birs-tree.appspot.com',
+  messagingSenderId: '366455834184'
+}
+
+Firebase.initializeApp(config)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
+
 })
